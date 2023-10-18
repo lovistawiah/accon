e1ead75d023b7d5b5cb10c828c8d5160ef3584a8
Lovis Tawiah <69266504+lovistawiah@users.noreply.github.com>
2023-10-18 14:35:00 +0000

    added filter page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import FilterPage from "@/components/FilterPage"
 import Header from "@/components/Header"
 import Tile from "@/components/Tile"
 export default function Home() {
   return (
-  <div>hello</div>
+  <section className="flex flex-col w-[100dvw] h-[100dvh]">
+    <Header/>
+    <FilterPage/>
+  </section>
   )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,10 @@
 const Header = () => {
   return (
-   <nav>
+   <nav className="w-full h-[52px] flex py-0 px-[5px] justify-between items-center bg-white border-b border-slate-300">
+    <a className="text-[40px] text-sky-900 no-underline">
+      Accon
+    </a>
+    <button className="text-[16px] text-white bg-sky-800 hover:bg-sky-900 active:bg-sky-950 w-[112px] h-[37px] rounded-[5px] text-center">Sign up</button>
 </nav>
   )
 }
